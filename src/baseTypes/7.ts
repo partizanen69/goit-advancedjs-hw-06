43144ee370d7d67df473e728031b78f6a1e2366d
Oleksii Ablitsov <oleksii.ablitsov@bringg.com>
2023-12-05 18:03:35 +0200

    done

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,20 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum WeekDay {
+  monday = 0,
+  tuesday = 1,
+  wednesday = 2,
+  thursday = 3,
+  friday = 4,
+  saturday = 5,
+  sunday = 6,
+}
+
+const isWeekend = (day: WeekDay): boolean => {
+  return day > 4;
+};
+
+console.log(isWeekend(WeekDay.friday), "should be false");
+console.log(isWeekend(WeekDay.saturday), "should be true");
--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,8 +3,8 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
+function merge(objA: Record<string, any>, objB: Record<string, any>) {
   return Object.assign(objA, objB);
 }
 
-export {}
+export {};
